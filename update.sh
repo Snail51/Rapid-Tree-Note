abea0bca6bc28f2cf6b0ba5a6c1d3b60314701ec
mobren11 <mobren11@gmail.com>
2025-04-20 15:51:32 -0500

    make `update.sh` work
    `update.sh` addresses all shortcomings and consumes `setup.sh`. clones the git repo, installs replacement content, explicitly grabs the `.htaccess` file, and sets up all permissions

--- update.sh
+++ update.sh
@@ -3,9 +3,20 @@
 
 location=$(dirname $(realpath "$0"))
 
+# Retrieve Files From Repo
 rm -rvf "/tmp/Rapid-Tree-Note"
 git clone "https://github.com/Snail51/Rapid-Tree-Note.git" "/tmp/Rapid-Tree-Note"
 cp -rvf /tmp/Rapid-Tree-Note/* "$location"
 rm -rvf "/tmp/Rapid-Tree-Note"
+cd $location
+wget "https://raw.githubusercontent.com/Snail51/Rapid-Tree-Note/refs/heads/main/.htaccess" # why cant we copy .htaccess files normally?
 
-"${location}/setup.sh";
+# Change Base Permissions
+chown -R snail "$location"
+chgrp -R www-data "$location"
+chmod -R 0750 "$location"
+
+#Change Usage Permissions
+chmod -R 0700 "${location}/Usage"
+chmod 0750 "${location}/Usage"
+chmod -R 0720 "${location}/Usage/accesses.csv"
